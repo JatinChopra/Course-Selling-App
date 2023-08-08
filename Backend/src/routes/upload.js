41fd3e4dafd1d5a5b7c671122bf2e25ea3dabfbc
JatinChopra <jatinchopra2053@gmail.com>
2023-08-08 05:38:47 +0530

    fix(backend): serviceAccountkey change in upload.js

--- Backend/src/routes/upload.js
+++ Backend/src/routes/upload.js
@@ -4,9 +4,23 @@ require("dotenv").config();
 const multer = require("multer");
 const upload = multer({});
 
-var serviceAccount = require("../../serviceAccountKey.json");
+// var serviceAccount = require("../../serviceAccountKey.json");
 const { getStorage } = require("firebase-admin/storage");
 const verifyJWT = require("../middlewares/verifyJWT");
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_id,
+  private_key: process.env.private_key,
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url,
+  universe_domain: process.env.universe_domain,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
